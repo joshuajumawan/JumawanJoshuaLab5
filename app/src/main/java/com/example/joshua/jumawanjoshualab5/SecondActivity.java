7ac31a9725145a2b7dae1b10bc5fc1ade559fc84
joshuajumawan <rommeljoshua.jumawan.iics@ust.edu.ph>
2018-10-08 22:51:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/joshua/jumawanjoshualab5/SecondActivity.java
@@ -0,0 +1,75 @@
+package com.example.joshua.jumawanjoshualab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent i = new Intent(this, MyService.class);
+        startService(i);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("4ITF", "onStart has executed...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("4ITF", "onResume has executed...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("4ITF", "onPause has executed...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("4ITF", "onStop has executed...");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("4ITF", "onRestart has executed...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("4ITF", "object is deallocated...");
+    }
+
+    public void activity1(View v){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+    public void activity2(View v){
+        Intent i = new Intent(this,SecondActivity.class);
+        startActivity(i);
+    }
+
+    public void map(View v){
+        Intent i = null, chooser=null;
+        if (v.getId()== R.id.mapbtn) {
+            i=new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.5820293,120.9835931"));
+            chooser= Intent.createChooser(i, "Select your Map App");
+            startActivity(chooser);
+        }
+
+    }
+}
